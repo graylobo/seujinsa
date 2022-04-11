c97ed1b6824e96166080aebba505bc8085e77883
graylobo <cazador7@naver.com>
2022-04-12 03:55:03 +0900

    timerSelector 추가 (타이머 조건부 설정)

--- src/common/components/BuildAlert.tsx
+++ src/common/components/BuildAlert.tsx
@@ -1,61 +1,50 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useRecoilState } from "recoil";
-import { timerComponentCount } from "../recoil/states";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import {
+  timerState,
+  timerRunningState,
+  buildAlertLanguage,
+} from "../recoil/states";
 import TimerComponent from "./TimerComponent";
-interface OptionProp {
-  rate?: number;
-  pitch?: number;
-  lang: string;
-}
 
-function speak(text: string, opt_prop: OptionProp) {
-  if (
-    typeof SpeechSynthesisUtterance === "undefined" ||
-    typeof window.speechSynthesis === "undefined"
-  ) {
-    alert("이 브라우저는 음성 합성을 지원하지 않습니다.");
-    return;
-  }
-
-  window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
-
-  const prop = opt_prop || {};
-  const speechMsg = new SpeechSynthesisUtterance();
-  speechMsg.rate = prop.rate || 1; // 속도: 0.1 ~ 10
-  speechMsg.pitch = prop.pitch || 1; // 음높이: 0 ~ 2
-  speechMsg.lang = prop.lang || "ko-KR";
-  speechMsg.text = text;
-
-  // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-  window.speechSynthesis.speak(speechMsg);
-}
 export default function BuildAlert() {
-  const [selectedLang, setLang] = useState("");
-  const [state, setState] = useRecoilState(timerComponentCount);
-  const nextId: number = state.length > 0 ? state[state.length - 1].id + 1 : 0;
+  const [timer, setTimer] = useRecoilState(timerState);
+  const [timerRunning, setTimerRunning] = useRecoilState(timerRunningState);
+  const [lang, setLang] = useRecoilState(buildAlertLanguage);
+  const nextId: number = timer.length > 0 ? timer[timer.length - 1].id + 1 : 0;
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          setState([...state, { id: nextId, content: "" }]);
-        }}
-      >
-        타이머 추가
-      </button>
-      {state.map((e) => (
-        <TimerComponent id={e.id}></TimerComponent>
-      ))}
-      <button onClick={() => {}}>시작</button>
-      <select
-        id="select-lang"
-        onChange={(e) => {
-          setLang(e.target.value);
-        }}
-      >
-        <option value="ko-KR">한국어</option>
-        <option value="en-US">영어</option>
-      </select>
+    <div className="flex flex-col mt-[50px]">
+      <div className=" self-center">
+        <button
+          onClick={() => {
+            setTimer([...timer, { id: nextId, content: "" }]);
+          }}
+        >
+          타이머 추가
+        </button>
+        {timer.map((e, i) => (
+          <div className="mb-[10px]" key={i}>
+            <TimerComponent id={e.id}></TimerComponent>
+          </div>
+        ))}
+        <button
+          onClick={() => {
+            setTimerRunning(!timerRunning);
+          }}
+        >
+          {timerRunning ? "중지" : "시작"}
+        </button>
+        <select
+          id="select-lang"
+          onChange={(e) => {
+            setLang({ lang: e.target.value });
+          }}
+        >
+          <option value="ko-KR">한국어</option>
+          <option value="en-US">영어</option>
+        </select>
+      </div>
     </div>
   );
 }
--- src/common/recoil/states.ts
+++ src/common/recoil/states.ts
@@ -1,19 +1,59 @@
-import { Interface } from "readline";
-import { atom } from "recoil";
+import { atom, DefaultValue, selector } from "recoil";
+import { DefaultProps } from "vue/types/options";
+import {OptionProp} from "../utils/tts"
 
+
+export interface ITimerProps{
+  id:number,
+  content:string,
+  hour?:string,
+  minute?:string,
+  second?:string
+}
 const menuState = atom<boolean>({
   key: "menuState",
   default: false,
 });
 
+const timerRunningState = atom<boolean>({
+  key:"timerRunningState",
+  default:false
+})
 
- export interface ITimerProps{
-  id:number,
-  content?:string
-}
-const timerComponentCount = atom<ITimerProps[]>({
-  key: "timerComponentCount",
+const buildAlertLanguage=atom<OptionProp>({
+  key:"buildAlertLanguage",
+  default:{lang:"ko-KR"}
+})
+
+const timerState = atom<ITimerProps[]>({
+  key: "timerState",
   default: [],
 });
 
-export { menuState, timerComponentCount };
+const timerSelector = selector<ITimerProps[]>({
+  key:"timerSelector",
+  get:({get})=>{
+    return get(timerState);
+  },
+  set:({set},timer)=>{
+    set(timerState,(prev)=>{
+      timer = timer.map((e)=>{
+        if(e.hour>=24){
+          return {...e,hour:23}
+        }
+        if(e.minute>=60){
+          return {...e,minute:59}
+        }
+        if(e.second>=60){
+          return {...e,second:59}
+        }
+        else{
+          return e
+        }
+      })
+      return [...timer]
+    })
+
+  }
+})
+export { menuState, timerState,timerRunningState,buildAlertLanguage,timerSelector };
