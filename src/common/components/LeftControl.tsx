b183e7c1963aba1a1f321420c2ea8521d988aebf
graylobo <cazador7@naver.com>
2022-04-12 16:26:51 +0900

    feat:왼손생산 예외처리 기능 추가

--- src/common/components/LeftControl.tsx
+++ src/common/components/LeftControl.tsx
@@ -1,11 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 
-function highLight(word: string, className: string): string {
-  let innerHTML = "";
-  innerHTML += `<span class='${className}'>${word}</span>`;
-  return innerHTML;
-}
-
 let combo = 0;
 export default function LeftControl() {
   const [keyword, setKeyword] = useState("");
@@ -13,8 +7,11 @@ export default function LeftControl() {
   const [inputWord, setInputWord] = useState("");
   const [checkBox, setCheckBox] = useState(true);
   const [comboUp, setComboUp] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
   const keywordValue = useRef<any>();
   const inputValue = useRef<any>();
+
+  // 키워드 입력 전부 일치시 input 초기화 후 마지막 단어 설정
   if (savedWord.length < inputWord.length && savedWord.length !== 0) {
     inputValue.current.value = inputWord[inputWord.length - 1];
     setInputWord(inputValue.current.value);
@@ -26,18 +23,31 @@ export default function LeftControl() {
       if (savedWord === inputWord) {
         combo += 1;
         setComboUp(true);
+        setTimeout(() => {
+          setComboUp(false);
+        }, 100);
       }
     }
   }, [inputWord]);
+
+  useEffect(() => {
+    inputValue.current.value = "";
+    setInputWord("");
+    combo = 0;
+  }, [isSaved]);
+
   return (
-    <div className="mt-[100px] flex justify-center  ">
-      <div className="relative border-2 border-black-600 rounded-[30px]  p-[30px] h-[350px] w-[700px] flex justify-center flex-col items-center">
-        <div className="relative">
+    <div className="mt-[100px] border-2 border-black-600 rounded-[30px] h-[350px] min-w-[600px] flex flex-col">
+      <div className="relative p-[30px] self-center  flex flex-col ">
+        <div className="relative self-center mb-[10px]">
           <div className="">키워드 입력 (30/{keyword.length}자)</div>
           <input
             type="text"
-            className=" w-[300px] h-[50px] border-2 border-black p-[10px] rounded-[3px] outline-blue-600"
+            className={`w-[300px] h-[50px] border-2 border-black p-[10px] rounded-[3px] outline-blue-600 ${
+              isSaved ? " bg-gray-400" : ""
+            }`}
             placeholder="왼손 생산할 키워드 입력"
+            readOnly={isSaved}
             ref={keywordValue}
             onChange={(e) => {
               if (e.target.value.length > 30) {
@@ -60,35 +70,24 @@ export default function LeftControl() {
                 keywordValue.current.value = "";
               } else {
                 setSavedWord(keywordValue.current.value);
+                setIsSaved(!isSaved);
               }
             }}
           >
-            저장
+            {isSaved ? "수정" : "저장"}
           </button>
         </div>
-        <div className="text-[30px] h-[100px]">
-          {savedWord}
-          <div>
-            {inputWord.split("").map((e, i) => {
-              if (e === savedWord[i]) {
-                return <span className="text-blue-600 text-[30px]">{e}</span>;
-              } else {
-                if (savedWord.length > i && savedWord[i] !== undefined) {
-                  if (checkBox) {
-                    inputValue.current.value = "";
-                  }
-                  combo = 0;
-                }
-                return <span className="text-red-600 text-[30px]">{e}</span>;
-              }
-            })}
-          </div>
-        </div>
-        <div className="relative ">
+        <div className="relative self-center ">
           <input
             type="text"
-            className="w-[300px] h-[50px] rounded-[3px] border-2 border-black  p-[10px]   outline-blue-600"
+            className={`w-[300px] h-[50px] rounded-[3px] border-2 border-black  p-[10px] outline-blue-600 ${
+              !isSaved ? "bg-gray-400" : ""
+            }`}
             ref={inputValue}
+            readOnly={!isSaved}
+            onPaste={(e) => {
+              e.preventDefault();
+            }}
             onChange={(e) => {
               if (savedWord.length === 0) {
                 alert("입력할 키워드를 설정해야 합니다.");
@@ -112,10 +111,29 @@ export default function LeftControl() {
             </label>
           </div>
         </div>
+        <div className="text-[24px] h-[80px] self-center ">
+          {savedWord}
+          <div>
+            {inputWord.split("").map((e, i) => {
+              if (e === savedWord[i]) {
+                return <span className="text-blue-600 text-[24px]">{e}</span>;
+              } else {
+                if (savedWord.length > i && savedWord[i] !== undefined) {
+                  if (checkBox) {
+                    inputValue.current.value = "";
+                  }
+                  combo = 0;
+                }
+                return <span className="text-red-600 text-[24px]">{e}</span>;
+              }
+            })}
+          </div>
+        </div>
+
         <div
-          className={` absolute bottom-[15px] ${
+          className={`bottom-[15px] self-center  ${
             comboUp
-              ? " transition-text duration-[0.1s] text-[20px]"
+              ? " transition-text duration-[0.1s] text-[20px] text-red-600 font-bold"
               : "transition-text duration-[0.1s] text-[17px] "
           }`}
         >
