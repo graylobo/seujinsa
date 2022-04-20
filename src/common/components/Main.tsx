9aae74ead23c96d397275f8a1d27f38172d9d7f1
graylobo <cazador7@naver.com>
2022-04-20 20:02:41 +0900

    메인화면 유투브 영상으로 변경

--- src/common/components/Main.tsx
+++ src/common/components/Main.tsx
@@ -1,11 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { userInfoState, logoutState } from "../recoil/states";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import YouTube from "react-youtube";
+
 export default function Main() {
   const userInfo = useRecoilValue(userInfoState);
   const [logout, setLogout] = useRecoilState(logoutState);
+  const [isMute, setMute] = useState(false);
+  const muteRef = useRef<any>();
+  // const [width, setWidth] = useState<number>(window.innerWidth);
+
+  // 모바일 사이즈인지 체크하는 함수
+  // function handleWindowSizeChange() {
+  //   setWidth(window.innerWidth);
+  // }
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleWindowSizeChange);
+  //   return () => {
+  //     window.removeEventListener("resize", handleWindowSizeChange);
+  //   };
+  // }, []);
+
+  // const isMobile = width <= 768;
   useEffect(() => {
     if (logout) {
       toast.info("로그아웃 되었습니다.", {
@@ -15,10 +33,58 @@ export default function Main() {
       setLogout(false);
     }
   }, [logoutState]);
+  const opts = {
+    width: "100%",
+    height: "100%",
+    playerVars: {
+      controls: 0,
+      autoplay: 1,
+      loop: 1,
+      playlist: "A5AmE_b68cg",
+    },
+  };
+
   return (
     <div className="mt-[30px]">
       <ToastContainer />
-      <img src="/star-cat.jpg" alt="" />
+      <div className="absolute w-full h-full top-0 left-0 ">
+        <div className="absolute   w-full h-full top-0 left-0 "></div>
+        <YouTube
+          videoId="A5AmE_b68cg"
+          opts={opts}
+          // containerStyle={styles.promAlert}
+          containerClassName={"h-full"}
+          onReady={(e) => {
+            e.target.playVideo();
+            muteRef.current = e.target;
+          }}
+        />
+        {/* <img src="/star-cat.jpg" alt="" /> */}
+
+        <div className="flex justify-center">
+          {isMute ? (
+            <button
+              onClick={() => {
+                muteRef.current.unMute();
+                setMute(false);
+              }}
+              className="absolute z-50 my-auto bottom-[60px] text-white"
+            >
+              음소거 해제
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                muteRef.current.mute();
+                setMute(true);
+              }}
+              className="absolute z-50 my-auto bottom-[60px] text-white"
+            >
+              음소거
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/common/components/Navigation.jsx
+++ src/common/components/Navigation.jsx
@@ -4,7 +4,7 @@ export default function Navigation({ setMenu }) {
   return (
     <div className=" relative ">
       <Link href={"/"}>
-        <span className="text-[30px] cursor-pointer subject absolute left-[0px] ">
+        <span className="text-[30px] cursor-pointer subject absolute left-[0px] text-white z-10 ">
           스<span className="text-[18px]">타에</span>진
           <span className="text-[18px]">심인</span>사
           <span className="text-[18px]">이트</span>
@@ -12,7 +12,7 @@ export default function Navigation({ setMenu }) {
       </Link>
 
       <span
-        className="material-icons-outlined cursor-pointer absolute right-[0px] text-[30px] "
+        className="material-icons-outlined cursor-pointer absolute right-[0px] top-[10px] text-[30px] text-white z-10"
         onClick={() => {
           setMenu((e) => !e);
         }}
