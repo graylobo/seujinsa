d7c7d50783aeb134648fd0d123196c85b4a3cfa6
graylobo <cazador721@daum.net>
2022-04-25 13:45:06 +0900

    toast 모듈화

---
+++ src/common/utils/toast.js
@@ -0,0 +1,11 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function success(msg, autoClose = 1500) {
+  toast.success(msg, {
+    autoClose: autoClose,
+    position: toast.POSITION.TOP_CENTER,
+  });
+}
+
+export { success };
