2ab5f64169593452b83eefce5d503bc770e4b820
graylobo <cazador7@naver.com>
2022-04-25 09:21:53 +0900

    날짜표시 버그 수정

--- src/common/components/footer/QNA.tsx
+++ src/common/components/footer/QNA.tsx
@@ -18,10 +18,11 @@ function getDateFormat(): string {
   let date: any = new Date();
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
-  let day = date.getDay();
+  let day = date.getDate();
   let hour = date.getHours();
   let minute = date.getMinutes();
   let second = date.getSeconds();
+  console.log(day);
   month = month >= 10 ? month : "0" + month;
   day = day >= 10 ? day : "0" + day;
   hour = hour >= 10 ? hour : "0" + hour;
