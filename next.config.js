700cce0e2c9a3132fbca806818069b1103152ca5
graylobo <cazador7@naver.com>
2022-05-02 18:54:35 +0900

    cors 에러 우회 proxy 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `https://seujinsa.herokuapp.com/:path*`,
+      },
+    ];
+  },
   images: {
     domains: ["https://seujinsa.netlify.app/", "localhost"],
   },
