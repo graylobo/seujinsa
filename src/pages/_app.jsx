e84b600facea117e2cc650a3fc72081cbbc72041
graylobo <cazador7@naver.com>
2022-08-20 14:37:58 +0900

    robots.tsx , 사이트맵, og 태그 설정

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>스타티어표</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+       
         <link rel="icon" href="/staricon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <link
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,31 +4,21 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="keyword" content="티어표, 스진사, 스타크래프트, 스타티어표, 스타티어, 티어, 스타대학, 스타대학교"></meta>
+        <meta name="description" content="티어표, 스진사, 스타크래프트, 스타티어표, 스타티어, 티어, 스타대학, 스타대학교"></meta>
 
-        <meta
-          name="keyword"
-          content="티어표, 스진사, 스타크래프트, 스타티어표, 스타티어, 티어, 스타대학, 스타대학교"
-        ></meta>
-        <meta
-          name="description"
-          content="티어표, 스진사, 스타크래프트, 스타티어표, 스타티어, 티어, 스타대학, 스타대학교"
-        ></meta>
-        <meta
-          name="naver-site-verification"
-          content="547764876549458d441fe09377c3fd122d74e96b"
-        />
+        <meta property="og:title" content="스진사" />
+        <meta property="og:description" content="티어표, 스진사, 스타크래프트, 스타티어표, 스타티어, 티어, 스타대학, 스타대학교" />
+        <meta property="og:url" content="https://seujinsa.com" />
+        <meta property="og:image" content="https://downloadwap.com/thumbs2/wallpapers/p2ls/2019/misc/45/s9lop99613103199.jpg" />
+        <meta name="naver-site-verification" content="547764876549458d441fe09377c3fd122d74e96b" />
 
-        <meta
-          name="google-site-verification"
-          content="4q1tSg31U51-TgnZXjup14H9acNB7TuV-yHWC9g5X4s"
-        />
+        <meta name="google-site-verification" content="4q1tSg31U51-TgnZXjup14H9acNB7TuV-yHWC9g5X4s" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1544015487048934"
           crossOrigin="anonymous"
         ></script>
-
-       
 
         <body>
           <Main />
