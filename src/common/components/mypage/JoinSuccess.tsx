c7002d2a33e1b5bbf2c25b486c78111578269ea0
Seongjun Min <cazador7@naver.com>
2022-12-08 00:18:58 +0900

    rebuild

--- src/common/components/mypage/JoinSuccess.tsx
+++ src/common/components/mypage/JoinSuccess.tsx
@@ -9,7 +9,7 @@ export default function JoinSuccess() {
           이메일 인증이 성공적으로 완료되었습니다.
         </div>
         <Link href={"/signin"}>
-          <button className="mt-[30px] h-[48px] w-full py-[12px] rounded-[8px] outline-none transition-colors bg-[#e13431] disabled:bg-gray-50 text-white hover:bg-red-700">
+          <button className="mt-[30px] h-[48px] w-full py-[12px]  rounded-[8px] outline-none transition-colors bg-[#e13431] disabled:bg-gray-50 text-white hover:bg-red-700">
             로그인 하기
           </button>
         </Link>
